cded92b6daffc9727ddd11041fe68775a3bf28cb
apercebo <apercebo@student.42lyon.fr>
2022-05-24 17:29:55 +0200

    bonjour

---
+++ pipex.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apercebo <apercebo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 14:16:34 by apercebo          #+#    #+#             */
+/*   Updated: 2022/05/20 14:46:56 by apercebo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+
+int	main(int argc, char *argv[])
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	pipe(fd);
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		execve("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+
+	waitpid(pid1, NULL, 0);
+	return (0);
+}
